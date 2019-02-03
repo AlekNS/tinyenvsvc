29c71234cad4674750b97e7cfe695b159980c212
Alexander Sedelnikov <alexander.sedelnikov@gmail.com>
2019-02-03 22:37:49 +0700

    Fix cors.

--- backend/auth/auth/app.py
+++ backend/auth/auth/app.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from sanic import Sanic
 from dotenv import load_dotenv
+from sanic_cors import CORS
 from .errors import CustomErrorHandler
 
 
 load_dotenv()
 
 app = Sanic('auth', load_env='AUTH_')
+cors = CORS(app, resources=r"/api/*", automatic_options=True)
 app.error_handler = CustomErrorHandler()
